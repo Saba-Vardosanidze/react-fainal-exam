eedeb680120561d1507ccb0fc4b07bc693fa7391
Saba-Vardosanidze <sabavardo674@gmail.com>
2025-05-19 23:59:06 +0400

    update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import {useState} from "react";
 import styles from "./Card.module.css";
 import disslike from "/src/assets/dislike.png";
 import like from "/src/assets/Like.png";
